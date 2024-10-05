3d77b4353e4cd34279dde3493fd03568ae0f4d31
FrankNijhof <frank_nijhof_20@hotmail.com>
2024-10-05 18:24:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# Getting Started with Create React App
+# Object Calculator
+
+## About this project
+As a person who plays tabletop Warhammer games, I need to make a list with units with each of them a point value. The Warhammer app provides a tool that to realise this, but I can only make one list on the free version. However, I want to make multiple lists and I don't need the extra features that the Warhammer app provides. I simply need something that can count the unit points in total from a list, edit the list by changing name of the list, adding a new item to an existing list, editing the (points) value, creating new or removing existing lists.
+
+You might think to yourself: "But if you want to create a list, just use a text file and a calculator.". You are correct! HOWEVER... I am using this as an oppurtunity to learn React which is the main purpose of this project. And hopefully that I this app can be used in the future for personal use.
+
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Built with
+
+* Reactjs
+* Material UI
+
+## Tools
+
+* Visual Studio Code
+
+## Current features
+
+### Show list with its items
+
+Able to show a list with its items. It shows the name and value of each item and count the total of the list.
+
+![Alt preview showing list](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGZqbHB2cnI0ZTk0ajVlbnhub3dodTRxaWF1cDZzZWY2MzFjNHNzeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DbfAB5J3WlvvgW6lJy/giphy.gif)
+
+
+Currently you can only view the list. The next steps for this feature
+
+
+## Todo list
+
+### FRONTEND
+TODO: show list items visually that they are connected/related to a colapsed list
+TODO: creating a new list, the popup should look bigger
+
+### BACKEND
+TODO CREATE: new (empty) list
+TODO UPDATE: list with updated with added items
+TODO UPDATE: change an existing list name
+TODO UPDATE: change an existing list item name
+TODO DELETE: existing list
+TODO DELETE: existing list item 
 
 ## Available Scripts
 
@@ -38,33 +79,3 @@ If you aren't satisfied with the build tool and configuration choices, you can `
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
 You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,190 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { useState } from 'react';
+
 import './App.css';
+import data from './userdata/list_of_objects.json';
+import Button from '@mui/material/Button';
+import { Dialog, DialogTitle, List, ListItemButton, Collapse, ListItemText, TextField, ListItem } from '@mui/material';
+import DialogActions from '@mui/material/DialogActions';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+//*****FRONTEND***** */
+//TODO: show list items visually that they are connected/related to a colapsed list
+//TODO: creating a new list, the popup should look bigger
+
+//******BACKEND******
+//TODO CREATE: new (empty) list
+//TODO UPDATE: list with updated with added items
+//TODO UPDATE: change an existing list name
+//TODO UPDATE: change an existing list item name
+//TODO DELETE: existing list
+//TODO DELETE: existing list item 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Object calculator</h1>
+       <CreateListComponent/>
+       <ListOfObjectsComponent/>
     </div>
   );
+}
+
+
+
+function CreateListComponent()
+{
+  const [listName, setListName] = useState("");
+  const handleSubmit = (Event) =>{
+    
+  }
+
+  const [open, setOpen] = useState(false);
+
+  function HandleClick(){
+    setOpen(!open);
+  }
+//https://react.dev/reference/react-dom/components/form#noun-labs-1201738-(2)
+  return(
+    <div>
+            <Button variant='contained' onClick={HandleClick}>Create new list</Button>
+      <Dialog
+      open={open}
+      onClose={HandleClick}
+      >
+    <form>
+      <label>Enter list name
+      <input type='text' value={listName} onChange={(e) => setListName(e.target.value)}/>
+      </label>
+      <Button title='submit' type='submit'>Create list</Button>
+    </form>
+    </Dialog>
+    </div>
+
+  );
+}
+
+/*
+function CreateListComponent() {
+  //TODO: redo the handle click function
+  //TODO: rework dialog
+  const [open, setOpen] = useState(false);
+  const [listName, setListName] = useState("");
+  const handleChange = e => {
+    setListName(e.target.value);
+  };
+  function HandleClick(){
+    setOpen(!open);
+  }
+  return (
+    <div>
+      <Button variant='contained' onClick={HandleClick}>Create new list</Button>
+      <Dialog
+      open={open}
+      onClose={HandleClick}
+      >
+        <DialogTitle>Enter a list name</DialogTitle>
+        <TextField value={listName} onChange={handleChange} required={true} hint="New army"></TextField>
+        <DialogActions>
+        <Button onClick={CreateList(listName)} variant='contained'>Create</Button>
+        </DialogActions>
+      </Dialog>
+
+    </div>
+  );
+}
+
+
+
+function CreateList(listName)
+{
+  //Check if list has no "empty" value
+  //TODO add (empty) list to json file
+  //TODO handle that this event when CreateListComponent dialog has opened
+  if(listName === "") {
+    return(
+      console.log("Empty listname")
+    ); 
+  }
+  else {
+    return(
+      data.listWithObjects[listName]
+     ); 
+  }
+
+}*/
+
+function ListOfObjectsComponent()
+{
+  //fetch lists from json
+  return data.listWithObjects.map(obj => {
+    return(
+      <List key={obj.id}>
+     {ListItemComponent(obj)}
+     </List>
+    );
+  }
+  );
+}
+
+function ListItemComponent(obj)
+{
+  const [open, setOpen] = useState(false);
+
+  function handleClick(){
+    setOpen(!open);
+  }
+  //Check if a list has items
+  if(obj.listItems === undefined)
+  {
+    return(
+      <div key={obj.id}>
+      <ListItemButton key={obj.id} onClick={handleClick}>
+        <ListItemText primary={obj.listName}/>
+      </ListItemButton>
+    </div>
+    );
+  }
+  else
+  {
+    return(
+      <div key={obj.id}>
+      <ListItemButton onClick={handleClick}>
+        <ListItemText primary={obj.listName}/>
+        
+      </ListItemButton>  
+      <Collapse  in={open} unmountOnExit>
+        <List key={obj.id}>
+        {obj.listItems.map(item =>{
+        return (<ListItem key={item.id}>
+          <ListItemText primary={item.name}/>
+          <ListItemText primary={item.amount}/>
+          <ListItemText primary={item.value}/>
+        </ListItem>
+        )
+            }
+          )
+        }
+        Total: {GetTotalValue(obj)}
+        </List>
+      </Collapse>
+    </div>
+    );
+  }
+}
+
+//Get the value of all items tallied up
+function GetTotalValue(objectList)
+{
+  var totalValue = 0;
+
+  objectList.listItems.map(item =>{
+    totalValue  = totalValue + JSON.parse(item.value);
+  });
+
+ return totalValue;
+
 }
 
 export default App;
